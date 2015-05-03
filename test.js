ed2b599041b810011fcf6e46d1521644c5a085b7
tcandens <joey.thies@gmail.com>
2015-05-03 15:54:12 -0700

    Working pixel-transform and palette-transform test

--- test.js
+++ test.js
@@ -2,7 +2,10 @@
 
 var fs = require('fs');
 
-fs.readFile('./lib/bitmap1.bmp', function(error, data) {
+// var path = './lib/non-palette-bitmap.bmp';
+var path = './lib/non-palette-bitmap.bmp';
+
+fs.readFile(path, function(error, data) {
 
   if (error) console.log(error);
 
@@ -11,55 +14,78 @@ fs.readFile('./lib/bitmap1.bmp', function(error, data) {
 
   image.file_size = data.readUInt32LE(2);
 
-  image.header_size = image.palette_location = data.readUInt32LE(14);
+  image.header_size = data.readUInt32LE(14);
 
   image.width = data.readInt32LE(18);
   image.height = data.readInt32LE(22);
 
+  image.pixel_depth = data.readUInt16LE(28);
+  // console.log(image['pixel_depth']);
+  image.compression = data.readInt32LE(30);
+  // console.log(image['compression']);
+
   image.image_size = data.readInt32LE(34); // should eql file_size - image_address
   image.palette_size = data.readInt32LE(46);
   image.palette_used = data.readInt32LE(50);
-  console.log(image['palette_used']);
+  // console.log(image['palette_used']);
 
   image.image_address = data.readInt32LE(10);
   // console.log(image['image_address']);
 
-  image.palette = data.slice( 54, ( 54 + image['palette_size'] ) ); // Eqls 265, means 64 colors 4bytes each
+  // image.palette = data.slice( 54, ( 54 + image['palette_size'] ) ); // Eqls 265, means 64 colors 4bytes each
   // console.log(image['palette'].length + " / " + image['palette_size']); // EQUIVALENT!
+  // image.image = data.slice( image['image_address'], ( image['image_address'] + image['image_size'] ) );
 
-  image.image = data.slice( image['image_address'], ( image['image_address'] + image['image_size'] ) );
+  function accessPixels(buffer) {
+    var newBuffer = new Buffer(buffer.length);
+    // console.log(buffer.length);
+    buffer.copy(newBuffer);
+    // console.log(newBuffer.length);
+    var offset = image['image_address'];
+    var size = image['image_size'];
+    var pixel_array = [];
+    for (var i = 0; i < ( size / 3 ); i++) {
+      var cOffset = offset + ( i * 3 )
+      var b = newBuffer.readUInt8(cOffset);
+      var g = newBuffer.readUInt8(cOffset + 1);
+      var r = newBuffer.readUInt8(cOffset + 2);
+      // var a = newBuffer.readUInt8(cOffset + 3);
+      newBuffer.writeUInt8( ( 255 - b ), ( cOffset ) );
+      newBuffer.writeUInt8( ( 255 - g ), ( cOffset + 1) );
+      newBuffer.writeUInt8( ( 255 - r), ( cOffset + 2) );
+      // buffer.writeUInt8( ( 255 ), cOffset + 3);
+      var pixel = [];
+      pixel.push(r, g, b);
+      pixel_array.push(pixel);
+    }
+    // console.log(pixel_array);
+    return newBuffer;
+  };
 
   // console.log("File size (bytes): " + image['file_size']);
   // console.log("Dimensions: " + image['width'] + " x " + image['height']);
 
-
-  // Building Color Palette Array
-  var palette = [];
-  for ( var i = 0; i < ( image['palette'].length / 4 ); i++ ) { // Iterate over every 4th byte
-    var c = image['palette'].slice(i, (i + 4)); // Slice out 4 byte chunks for each color in palette
-    var r = c.readUInt8(0);
-    var g = c.readUInt8(1);
-    var b = c.readUInt8(2);
-    var a = c.readUInt8(3);
-    r = 255;
-    g = 255;
-    b = 255;
-    a = 255;
-    palette[i] = [];
-    palette[i].push( r, g, b, a );
-    // console.log( palette[i] );
+  function invertBuffer(buffer) {
+    // Inverts Colors in Color Palette
+    for (var i = 0; i < ( image['palette_size'] / 4 ); i++) {
+      var cOffset = 54 + ( i * 4 );
+      var b = buffer.readUInt8(cOffset + 0);
+      var g = buffer.readUInt8(cOffset + 1);
+      var r = buffer.readUInt8(cOffset + 2);
+      var a = buffer.readUInt8(cOffset + 3);
+      buffer.writeUInt8(( 255 - b ), (cOffset + 0));
+      buffer.writeUInt8(( 255 - g ), (cOffset + 1));
+      buffer.writeUInt8(( 255 - r ), (cOffset + 2));
+      buffer.writeUInt8(( 255 - a ), (cOffset + 3));
+      console.log('Palette color replaced!');
+    };
+    return newBuffer;
   };
 
-  for (var i = 0; i < ( image['palette_size'] / 4 ); i++) {
-    var cOffset = 54 + ( i * 4 );
-    data.writeUInt8(0, (cOffset + 0));
-    data.writeUInt8(0, (cOffset + 1));
-    data.writeUInt8(0, (cOffset + 2));
-    data.writeUInt8(0, (cOffset + 3));
-    console.log('Palette color replaced!');
-  }
+  var newThing = accessPixels(data);
+  // var newThing = invertBuffer(data);
 
-  fs.writeFile('./lib/new.bmp', data, function( err ) {
+  fs.writeFile('./lib/new.bmp', newThing, function( err ) {
     if (err) console.log(err);
     console.log('New bitmap!');
   });
--- test2.js
+++ test2.js
@@ -1,10 +1,30 @@
 'use strict';
 
 var fs = require('fs');
+var util = require('util');
+var eventEmitter = require('events').EventEmitter;
 
-fs.readFile('./lib/bitmap1.bmp', function(err, data) {
-  var identity = new Buffer(4);
-  fs.read(data, identity, 0, 2, 0, function(err, bytesRead, identBuffer) {
-    console.log(identBuffer.toString());
-  });
+var path = './lib/bitmap1.bmp';
+
+var readStream = fs.createReadStream(path);
+
+// readStream.on('readable', function() {
+//   var chunk = readStream.read();
+//   var identity = chunk.slice(0, 2).toString('ascii');
+//   var pixel_start = chunk.slice(10, 14).readInt32LE(0);
+//   console.log(pixel_start);
+//   // readStream.pipe(writeStream);
+// });
+
+readStream.on('open', function() {
+  readStream.pipe(writeStream);
+})
+
+// readStream.on('error', function(err) {
+//   // console.log(err);
+// });
+
+var writeStream = fs.createWriteStream('./lib/new_stream.bmp');
+writeStream.on('open', function() {
+  console.log('File created');
 });
